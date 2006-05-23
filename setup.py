007ce479646ba1c6fa8e0a26146648a0d7936d11
PJ Eby <pje@telecommunity.com>
2006-05-23 05:53:30 +0000

    Add package description, minor formatting fixes, and use "dev" version numbering.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/BytecodeAssembler@2142 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- setup.py
+++ setup.py
@@ -12,11 +12,27 @@ PACKAGE_NAME = "BytecodeAssembler"
 PACKAGE_VERSION = "0.0.1"
 PACKAGES = ['peak', 'peak.util']
 
+def get_description():
+    # Get our long description from the documentation
+    f = file('README.txt')
+    lines = []
+    for line in f:
+        if not line.strip():
+            break     # skip to first blank line
+    for line in f:
+        if line.startswith('.. contents::'):
+            break     # read to table of contents
+        lines.append(line)
+    f.close()
+    return ''.join(lines)
+
 setup(
     name=PACKAGE_NAME,
     version=PACKAGE_VERSION,
 
     description='Generate Python code objects by "assembling" bytecode',
+    long_description = get_description(),
+
     author="Phillip J. Eby",
     author_email="peak@eby-sarna.com",
     license="PSF or ZPL",
