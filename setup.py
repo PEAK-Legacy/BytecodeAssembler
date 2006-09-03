f1bf1e1d460c6927b192440425724be56f7f1a7b
PJ Eby <pje@telecommunity.com>
2006-09-03 18:25:54 +0000

    Bump revision #
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/BytecodeAssembler@2233 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 # Metadata
 PACKAGE_NAME = "BytecodeAssembler"
-PACKAGE_VERSION = "0.2"
+PACKAGE_VERSION = "0.3"
 PACKAGES = ['peak', 'peak.util']
 
 def get_description():
